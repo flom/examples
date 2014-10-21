1906470a9511fd491ec4b5266003d956346b52e7
Florian Mladitsch <florian.mladitsch@gmail.com>
2014-10-21 12:09:18 +0200

    example project for PriorityQueue

---
+++ PriorityQueue/PriorityQueue/HeapPriorityQueue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriorityQueue
+{
+  public class HeapPriorityQueue : IPriorityQueue
+  {
+    private int[] elements;
+
+    public HeapPriorityQueue(int maxNumber)
+    {
+      elements = new int[maxNumber];
+    }
+
+    public int Insert(int key)
+    {
+      throw new NotImplementedException();
+    }
+
+    public int ExtractMinimum()
+    {
+      throw new NotImplementedException();
+    }
+
+    public int FindMinimum()
+    {
+      throw new NotImplementedException();
+    }
+
+    public void DecreaseKey(int id, int newKey)
+    {
+      throw new NotImplementedException();
+    }
+
+    public int Number()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ PriorityQueue/PriorityQueue/IPriorityQueue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriorityQueue
+{
+  public interface IPriorityQueue
+  {
+    // inserts new key and returns unique ID associated to key
+    int Insert(int key);
+    // returns minimum key and removes it from queue
+    int ExtractMinimum();
+    // returns minimum key without removing it from queue
+    int FindMinimum();
+    // decreases key with given id
+    void DecreaseKey(int id, int newKey);
+    // returns number of elements in queue
+    int Number();
+  }
+}
