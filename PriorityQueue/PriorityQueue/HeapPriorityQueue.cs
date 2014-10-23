9066fe4fbcdb468f825a6ac92941216dd6c7d82b
Florian Mladitsch <florian.mladitsch@gmail.com>
2014-10-23 17:47:53 +0200

    implemented HeapPriorityQueue

--- PriorityQueue/PriorityQueue/HeapPriorityQueue.cs
+++ PriorityQueue/PriorityQueue/HeapPriorityQueue.cs
@@ -9,35 +9,116 @@ namespace PriorityQueue
   public class HeapPriorityQueue : IPriorityQueue
   {
     private int[] elements;
+    private int[] indicies;
+    private int length;
+    private int idCounter;
 
     public HeapPriorityQueue(int maxNumber)
     {
-      elements = new int[maxNumber];
+      elements = new int[maxNumber + 1]; // + 1 because index starts at 1
+      indicies = new int[maxNumber + 1];
+      elements[0] = -1; // index 0 not used
+      length = 0;
+      idCounter = 0;
     }
 
     public int Insert(int key)
     {
-      throw new NotImplementedException();
+      idCounter++;
+
+      elements[length + 1] = key;
+      indicies[length + 1] = idCounter;
+
+      HeapifyUp(length + 1);
+
+      length++;
+
+      return idCounter;
     }
 
     public int ExtractMinimum()
     {
-      throw new NotImplementedException();
+      int minimum = elements[1];
+
+      Swap(1, length);
+      length--;
+      HeapifyDown(1);
+
+      return minimum;
     }
 
     public int FindMinimum()
     {
-      throw new NotImplementedException();
+      return elements[1];
     }
 
     public void DecreaseKey(int id, int newKey)
     {
-      throw new NotImplementedException();
+      int index = 0;
+      for (int i = 1; i <= length; i++)
+      {
+        if (indicies[i] == id)
+        {
+          index = i;
+          break;
+        }
+      }
+
+      elements[index] = newKey;
+      HeapifyUp(index);
     }
 
     public int Number()
     {
-      throw new NotImplementedException();
+      return length;
+    }
+
+    private void HeapifyUp(int index)
+    {
+      if (index == 1) return;
+
+      int parent = index / 2;
+
+      if (elements[index] < elements[parent])
+      {
+        Swap(parent, index);
+
+        HeapifyUp(parent);
+      }
+    }
+
+    private void HeapifyDown(int index)
+    {
+      int left = index * 2;
+      int right = (index * 2) + 1;
+
+      int minimum = index;
+
+      if (left <= length && elements[left] < elements[minimum])
+      {
+        minimum = left;
+      }
+      if (right <= length && elements[right] < elements[minimum])
+      {
+        minimum = right;
+      }
+
+      if (minimum != index)
+      {
+        Swap(index, minimum);
+        HeapifyDown(minimum);
+      }
+    }
+
+    private void Swap(int index1, int index2)
+    {
+      int temp = elements[index1];
+      elements[index1] = elements[index2];
+      elements[index2] = temp;
+
+      temp = indicies[index1];
+      indicies[index1] = indicies[index2];
+      indicies[index2] = temp;
     }
   }
 }
--- PriorityQueue/PriorityQueue/Program.cs
+++ PriorityQueue/PriorityQueue/Program.cs
@@ -10,6 +10,20 @@ namespace PriorityQueue
   {
     static void Main(string[] args)
     {
+      var q = new HeapPriorityQueue(15);
+
+      int[] foo = new int[] { 25, 50, 20, 7, 30, 15, 5, 10, 1 };
+      for (int i = 0; i < foo.Length; i++)
+      {
+        q.Insert(foo[i]);
+      }
+
+      for (int i = 0; i < foo.Length; i++)
+      {
+        Console.WriteLine(q.ExtractMinimum());
+      }
+
+      Console.Read();
     }
   }
 }
